b5d623b5b39c674fc87f45ec86f55a6d6d63ff74
Natacha POQUE-LANDON <natacha.poque@hotmail.fr>
2023-08-23 16:05:33 +0200

    Ajout des commentaires de code

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -1,5 +1,5 @@
 
-// Fetch data from the API
+// Récupération des données depuis l'API
 fetch('http://localhost:5678/api/works', {
     method: 'GET',
     headers: {
@@ -7,14 +7,17 @@ fetch('http://localhost:5678/api/works', {
     }
 })
 .then(response => {
+    // Vérifier si la réponse n'est pas réussie
     if (!response.ok) {
-        // Crée une erreur si la réponse n'est pas "ok"
         throw new Error('Unexpected Error'); 
     }
+    // Analyser les données JSON de la réponse
     return response.json();
 })
 .then(data => {
+    // Itérer à travers chaque objet "work" dans le tableau de données
     data.forEach(work => {
+        // Créer des éléments HTML pour chaque "work"
         const galleryContainer = document.getElementById('gallery-container');
         const figure = document.createElement('figure');
         const img = document.createElement('img');
@@ -24,10 +27,13 @@ fetch('http://localhost:5678/api/works', {
         const figcaption = document.createElement('figcaption');
         figcaption.textContent = work.title;
 
+        // Ajouter l'élément "img" et "figcaption" à l'élément "figure"
         figure.appendChild(img);
         figure.appendChild(figcaption);
         galleryContainer.appendChild(figure);
     });
 })
+
+    // Gérer les erreurs lors de la récupération des données ou de l'analyse JSON
 .catch(error => {console.error('Error fetching data:', error);
 });
